805ec76bf4cf0938605d02b3d683bef250768125
dylsub <dylansubramanian@gmail.com>
2022-06-30 15:25:03 -0500

    Added powerup and highscore functionality

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // initialize kaboom context
 kaboom({
     width: 190,
-    height: 209,
+    height: 220,
     font: 'apl386o',
     background: [30, 30, 30],
     global: true,
@@ -12,11 +12,14 @@ kaboom({
 loadSprite('player', '/sprites/bee.png');
 loadSprite('background', '/sprites/checkerboard.png');
 loadSprite('enemy', '/sprites/starfish.png')
+loadSprite('powerup', '/sprites/enemy.png')
+
 
 scene("game", () => {
 
     const background = add([
         sprite('background'),
+        z(-1),
     ])
 
     const player = add([
@@ -24,11 +27,20 @@ scene("game", () => {
         pos(85.5, 85.5),
         rotate(0),
         origin("center"),
+        z(1),
     ])
 
     const SPEED = 100;
     let canPlayerMove = true;
     let points = 0;
+
+    let highscore = 0;
+    if (localStorage.getItem("highscore")) {
+        highscore = Number(localStorage.getItem("highscore"));
+    }
+    else {
+        highscore = 0;
+    }
 
     //##########################################################//
     //Movement
@@ -104,15 +116,23 @@ scene("game", () => {
     //###########################################################//
 
     function increaseScore() {
+        //Scoring
         points++;
+        if (points > highscore) {
+            highscore = points;
+            localStorage.setItem("highscore", highscore);
+        }
+        bestText.text = "Best: " + highscore;
         score.text = "Score: " + points;
+
+        //Moving Enemies and checking for collision
         enemies.forEach((enemy) => {
             if (enemy.pos.x == player.pos.x && enemy.pos.y == player.pos.y) {
                 loseGame();
                 return;
             }
             const moves = [];
-            let nextMove;
+            let nextMove; foif
             const posx = enemy.pos.x;
             const posy = enemy.pos.y;
 
@@ -155,20 +175,57 @@ scene("game", () => {
 
             nextMove = Math.floor(Math.random() * moves.length);
             if (moves.length > 0) enemy.moveTo(enemy.pos.x + moves[nextMove][0], enemy.pos.y + moves[nextMove][1])
-            if (enemy.pos.x == player.pos.x && enemy.pos.y == player.pos.y) {
-                loseGame();
-            }
+            if (enemy.pos.x == player.pos.x && enemy.pos.y == player.pos.y) loseGame();
         })
-        if (points % 5 == 0) {
-            let spawnLocation = Math.floor(Math.random() * spawnSpots.length);
-            const enemy = add([
-                sprite('enemy'),
-                pos(spawnSpots[spawnLocation][0], spawnSpots[spawnLocation][1]),
-                rotate(0),
-                origin("center"),
-            ]);
-            enemies.push(enemy);
+        if (points % 5 == 0) spawnEnemy();
+        if (points % 20 == 0) spawnPowerup();
+    }
+
+    function spawnEnemy() {
+        let spawnLocation = Math.floor(Math.random() * spawnSpots.length);
+        const enemy = add([
+            sprite('enemy'),
+            pos(spawnSpots[spawnLocation][0], spawnSpots[spawnLocation][1]),
+            rotate(0),
+            origin("center"),
+            z(2),
+        ]);
+        enemies.push(enemy);
+    }
+
+    function spawnPowerup() {
+        const powerupSpawnLocations = [];
+        let spawnIncrement = 0;
+        let spawnIncrementRow = 0;
+        for (let x = 0; x < 8; x++) {
+            spawnIncrement = 0;
+            for (let i = 0; i < 8; i++) {
+                const currentSpot = [28.5 + spawnIncrement, 28.5 + spawnIncrementRow];
+                let canSpawnHere = true;
+                enemies.forEach((enemy) => {
+                    if (enemy.pos.x == currentSpot[0] && enemy.pos.y == currentSpot[1]) {
+                        canSpawnHere = false;
+                    }
+                })
+                if (player.pos.x == currentSpot[0] && player.pos.y == currentSpot[1]) {
+                    canSpawnHere = false;
+                }
+                if (canSpawnHere) powerupSpawnLocations.push(currentSpot);
+                spawnIncrement += 19;
+            }
+            spawnIncrementRow += 19;
         }
+        let spawnLocation = Math.floor(Math.random() * powerupSpawnLocations.length);
+
+        if (powerupSpawnLocations.length > 0) {
+            const powerup = add([
+                sprite('powerup'),
+                pos(powerupSpawnLocations[spawnLocation][0], powerupSpawnLocations[spawnLocation][1]),
+                origin("center"),
+                z(0),
+            ])
+        }
+
     }
 
     function loseGame() {
@@ -179,6 +236,7 @@ scene("game", () => {
             scale(0.28),
             origin("center"),
             pos(95, 70),
+            z(10),
         ])
         const restart = add([
             text("Play Again?"),
@@ -186,7 +244,7 @@ scene("game", () => {
             origin("center"),
             area({ cursor: "pointer", scale: 0.2 }),
             pos(95, 100),
-
+            z(10),
         ])
         const home = add([
             text("Menu"),
@@ -194,7 +252,7 @@ scene("game", () => {
             origin("center"),
             area({ cursor: "pointer", scale: 0.2 }),
             pos(95, 120),
-
+            z(10),
         ])
         home.onClick(() => {
             go("start", {});
@@ -238,10 +296,17 @@ scene("game", () => {
         }),
         pos(0, 192),
         scale(0.25),
+        z(10),
     ])
 
-    //##############################################################//
-    //Enemies
+    const bestText = add([
+        text("Best: " + highscore, {
+            font: "apl386",
+        }),
+        pos(0, 208),
+        scale(0.15),
+        z(10),
+    ])
 
     const spawnSpots = [
         [28.5, 9.5], [47.5, 9.5], [66.5, 9.5], [85.5, 9.5], [104.5, 9.5], [123.5, 9.5], [142.5, 9.5], [161.5, 9.5],
@@ -250,18 +315,19 @@ scene("game", () => {
         [28.5, 180.5], [47.5, 180.5], [66.5, 180.5], [85.5, 180.5], [104.5, 180.5], [123.5, 180.5], [142.5, 180.5], [161.5, 180.5]
     ];
     const enemies = [];
-
 })
 
 scene("start", () => {
     const background = add([
         sprite('background'),
+        z(-1),
     ])
     const title = add([
         text("Bee Careful!"),
         scale(0.28),
         origin("center"),
         pos(95, 70),
+        z(10),
     ])
     const start = add([
         text("Start"),
@@ -269,7 +335,7 @@ scene("start", () => {
         origin("center"),
         area({ cursor: "pointer", scale: 0.25 }),
         pos(95, 100),
-
+        z(10),
     ])
     start.onClick(() => {
         go("game", {});
@@ -292,8 +358,7 @@ scene("start", () => {
 
 
 function start() {
-    go("start", {});
-
+    go("start");
 }
 
 start();
